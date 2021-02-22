7619d954c888a9a4fa87bd763bf2a961c733e83b
Tarshish0906 <k.oxygen08@gmail.com>
2021-02-22 15:27:24 +0900

    正解すると正しくループを抜け出すようにしました

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -8,15 +9,29 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("秘密の数字は... {}", secret_number);
+    loop {
+        println!("予想 =");
 
-    println!("予想 =");
+        let mut guess = String::new();
 
-    let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("読み込みに失敗しました");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("読み込みに失敗しました");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    println!("入力値: {}", guess);
+        println!("入力値: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("小さすぎ！"),
+            Ordering::Greater => println!("大きすぎ！"),
+            Ordering::Equal => {
+                println!("やったね！！！");
+                break;
+            }
+        }
+    }
 }
